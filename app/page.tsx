6ced9d0ab86bf86d9503b305ecc42965286541de
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-24 23:54:01 +0530

    Style the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,34 +7,38 @@ const Home = () => {
     <>
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center">
-            <p className="apply flex gap-2 text-lg font-medium text-primary">
+          <div className="flex flex-col justify-center animate-fade-in-left">
+            <p className="flex items-center gap-2 text-lg font-medium text-primary group">
               Shop Smarter, <span className="text-blue-600"> Save Bigger!</span>
               <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={16}
                 height={16}
+                className="group-hover:translate-x-2 transition-all duration-300"
               />
             </p>
-            <h1 className="mt-4 text-6xl leading-[72px] font-bold tracking-[-1.2px] text-gray-900;">
+            <h1 className="typewriter">
               Find the <span className="text-blue-600"> Best Deals,</span> Every
               Time!
             </h1>
-            <p className="mt-6">
+            <p className="mt-6 text-lg text-gray-700">
               Track price trends, analyze product insights, and find the best
               deals to make informed purchases. 🚀
             </p>
-            <Searchbar />
+            <div className="animate-fade-in">
+              <Searchbar />
+            </div>
           </div>
-          <CarouselHome />
+          <div className="animate-fade-in">
+            <CarouselHome />
+          </div>
         </div>
       </section>
-      <section
-        className="flex flex-col gap-10 px-6 md:px-20 py-24;
-"
-      >
-        <h2 className="text-secondary text-[32px] font-semibold;">Trending</h2>
+      <section className="flex flex-col gap-10 px-6 md:px-20 py-2">
+        <h2 className="text-gray-900 text-[32px] font-semibold animate-fade-in">
+          Trending Now
+        </h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16"></div>
       </section>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.8s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
