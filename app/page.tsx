036c26b0d2bbd107a1647738ccf3189d1c04a493
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-29 21:51:06 +0530

    Add logic for entering valid amazon product link

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24">
-        <div className="flex max-xl:flex-col gap-16">
+        <div className="flex max-[1400px]:flex-col gap-16">
           <div className="flex flex-col justify-center animate-fade-in-left">
             <p className="flex items-center gap-2 text-lg font-medium text-primary group">
               Shop Smarter, <span className="text-blue-600"> Save Bigger!</span>
--- components/CarouselHome.tsx
+++ components/CarouselHome.tsx
@@ -7,13 +7,13 @@ const CarouselHome = () => {
   const carouselImages = [
     { imgUrl: "/assets/images/p1.jpg", alt: "oven" },
     { imgUrl: "/assets/images/p2.jpg", alt: "bag" },
-    { imgUrl: "/assets/images/p3.jpg", alt: "air fryer" },
+    { imgUrl: "/assets/images/p3.png", alt: "laptop" },
     { imgUrl: "/assets/images/p4.jpg", alt: "smart watch" },
     { imgUrl: "/assets/images/p5.jpg", alt: "chair" },
   ];
 
   return (
-    <div className="relative max-w-[90%] md:max-w-[600px] w-full mx-auto border border-gray-300 rounded-xl shadow-sm">
+    <div className="relative w-full max-w-full sm:max-w-[90%] md:max-w-[600px] mx-auto border border-gray-300 rounded-xl shadow-sm">
       <Carousel
         showThumbs={false}
         autoPlay
@@ -30,7 +30,7 @@ const CarouselHome = () => {
               alt={image.alt}
               width={484}
               height={484}
-              className="w-full h-auto max-h-[400px] object-contain"
+              className="w-full h-auto max-h-[250px] sm:max-h-[350px] md:max-h-[400px] object-contain"
             />
           </div>
         ))}
